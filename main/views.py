d3a9e823387367cc276238f2943655f057189c86
Gleb Rudometov <javist.gr@gmail.com>
2019-06-16 16:42:20 +0500

    final

--- main/views.py
+++ main/views.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-<<<<<<< HEAD
 from main.models import DataManager, Everything, Company
 from openpyxl import load_workbook
 import numpy as np
@@ -40,7 +39,6 @@ def rating(request):
         return render(request, 'rating.html', context={ 'name': request.user.username, 'rows':rows })
     else:
         return render(request, 'rating.html', context={ 'name': 'Войти', 'rows':rows })
-=======
 from main.models import DataManager
 from openpyxl import Workbook, load_workbook
 from main.models import Everything
@@ -56,88 +54,14 @@ def about(request):
     return render(request, template_name = 'about.html', context={ 'name': request.user.username })
 
 def rating(request):
-    def getData(orgData):
-        orgProjectNames = set((list(map(lambda x: x.territory_name, orgData))))
-        maxCost = max(list(map(lambda x: x.money, orgData)))
-        maxAccCountInProc = max(list(map(lambda x: x.accident_weight, orgData)))
-
-        # print('orgData = '+ str(len(orgData)))
-        # print('orgProjectNames = '+ str(orgProjectNames))
-        # print('maxAccInProc = '+ str(maxAccCountInProc))  
-
-        result = []
-
-        for orgProjectName in orgProjectNames:
-            #print(orgProjectName)
-
-            projectRecords = list(filter(lambda x: x.territory_name == orgProjectName, orgData))            
-            
-            inProcAccRecords = list(filter(lambda x: x.accident_type == 'В процессе', projectRecords))          
-            #print('inProcAccRecords = ' + str(len(inProcAccRecords)))
-            
-            inResultAccRecords = list(filter(lambda x: x.accident_type == 'По результату', projectRecords))
-            #print('inResultAccRecords = ' + str(len(inResultAccRecords)))
-
-            # print(len(inResultAccRecords))
-            # print(list(map(lambda x: x.accident_weight, inResultAccRecords)))
-
-            # print(len(inProcAccRecords))
-            # print(list(map(lambda x: x.accident_weight, inProcAccRecords)))
-
-            projectData = [
-                projectRecords[0].money / maxCost * 100,
-                len(inProcAccRecords),
-                reduce(lambda a, x: a + x, list(map(lambda x: x.accident_weight, inProcAccRecords)), 0) / 1 if len(inProcAccRecords) == 0 else len(inProcAccRecords),
-                len(inResultAccRecords),
-                reduce(lambda a, x: a + x, list(map(lambda x: x.accident_weight, inResultAccRecords)), 0) / 1 if len(inResultAccRecords) == 0 else len(inResultAccRecords)
-            ]
-
-            #print(projectData)
-            result.append(projectData)
-        
-        maxInProcAccCount = max(list(map(lambda x: x[1], result)))
-        maxInResultAccCount = max(list(map(lambda x: x[3], result)))
-        for resultItem in result:
-            resultItem[1] = resultItem[1] / (maxInProcAccCount / 100)
-            resultItem[3] = resultItem[3] / (maxInResultAccCount / 100)
-
-        return result
-
-    from main.models import Everything 
-    def normalize():
-        result = []
-
-        data = Everything.objects.all()
-        tender = list(filter(lambda x: x.territory_name == 'Благоустройство 21', data))[0]
-        print(tender.money)
-
-        orgNames = list(filter(lambda x: x != '0', set(list(map(lambda x: x.executor_name, data)))))
-        print(orgNames)
-
-        for orgName in orgNames:
-            print(orgName)
-            orgData = list(filter(lambda x: x.executor_name == orgName, data))
-
-            ratingData = getData(orgData)
-            orgQuality = reduce(lambda a, x: a + x[0] / (x[1] * x[2] + x[3] * x[4]), ratingData, 0) / len(ratingData)
-
-            orgExperience = len(
-                list(
-                    filter(lambda z: z > tender.money * 0.7,
-                    set(list(map(lambda x: x.money, orgData))))
-                )
-            )
-
-            result.append([orgName, orgQuality, orgExperience])
-        
-        # Массив массивов со структурой [Название компании, Качество, Опыт]
-        print(result)
-        return result
-
-    normalize()
-    return render(request, template_name='rating.html', context={'name': request.user.username})
->>>>>>> 7f31af562b2b4cdf73dbda0c652138d622b5e7b9
-
+    rows = []
+    for item in Everything.objects.all():
+        a = (item.executor_name, count_rating(item))
+        rows += [a]
+    if request.user.is_authenticated:
+        return render(request, 'rating.html', context={'name': request.user.username, 'rows': rows})
+    else:
+        return render(request, 'rating.html', context={'name': 'Войти', 'rows': rows})
 def form(request):
     if request.user.is_authenticated:
         return render(request, 'form.html', context={'name': request.user.username})
